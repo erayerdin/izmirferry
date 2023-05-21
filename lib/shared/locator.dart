faeee0b8fb2c021b7e787e7363b839cf40a086fe
Eray Erdin <eraygezer.94@gmail.com>
2023-05-22 00:54:15 +0300

    add dio to service locator

--- lib/shared/locator.dart
+++ lib/shared/locator.dart
@@ -4,4 +4,26 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-void initLocator() {}
+import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:get_it/get_it.dart';
+
+void initLocator() {
+  //-----//
+  // Dio //
+  //-----//
+  GetIt.I.registerLazySingleton<DioCacheInterceptor>(
+    () => DioCacheInterceptor(
+      options: CacheOptions(
+        store: MemCacheStore(),
+      ),
+    ),
+  );
+
+  GetIt.I.registerLazySingleton<Dio>(
+    () => Dio()
+      ..interceptors.add(
+        GetIt.I.get<DioCacheInterceptor>(),
+      ),
+  );
+}
