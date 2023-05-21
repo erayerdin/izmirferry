7ecdf2248aadb1bff4b1f152e3713e1e0a2c4094
Eray Erdin <eraygezer.94@gmail.com>
2023-05-22 01:16:29 +0300

    add predefined constant stations

---
+++ lib/ferry/constants.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2023 Eray Erdin
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import 'package:izmirferry/ferry/data/models/station/station.model.dart';
+
+const List<Station> stations = [
+  Station(id: 1, name: 'Konak'),
+  Station(id: 2, name: 'Karşıyaka'),
+  Station(id: 3, name: 'Bostanlı'),
+  Station(id: 4, name: 'Pasaport'),
+  Station(id: 5, name: 'Alsancak'),
+  Station(id: 6, name: 'Göztepe'),
+  Station(id: 7, name: 'Üçkuyular'),
+  Station(id: 1019, name: 'Karantina'),
+];
