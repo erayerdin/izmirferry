1662497f50f174db78501ac587d4d367551c926f
Eray Erdin <eraygezer.94@gmail.com>
2023-05-22 00:13:08 +0300

    add trace log level

---
+++ lib/shared/logger.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 Eray Erdin
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import 'package:loggy/loggy.dart';
+
+const LogLevel _traceLevel = LogLevel('trace', 1);
+
+extension LogLevelExtension on Loggy {
+  void trace(dynamic message, Object? error, StackTrace? stackTrace) =>
+      log(_traceLevel, message, error, stackTrace);
+}
