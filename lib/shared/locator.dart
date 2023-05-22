1e87f709ccf5cf926d8bd4218992a5e99d20159a
Eray Erdin <eraygezer.94@gmail.com>
2023-05-22 12:26:50 +0300

    add IzdenizStationConverter

--- lib/shared/locator.dart
+++ lib/shared/locator.dart
@@ -4,10 +4,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:flutter_loggy_dio/flutter_loggy_dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:izmirferry/ferry/data/converters/station/station.converter.dart';
+import 'package:izmirferry/ferry/data/models/station/station.model.dart';
 import 'package:izmirferry/ferry/data/providers/schedule/schedule.provider.dart';
 
 void initLocator() {
@@ -34,6 +38,13 @@ void initLocator() {
   //-----------//
   GetIt.I.registerLazySingleton<ScheduleProvider>(
     () => IzdenizScheduleProvider(client: GetIt.I.get()),
+  );
+
+  //------------//
+  // Converters //
+  //------------//
+  GetIt.I.registerLazySingleton<Converter<Map<String, dynamic>, Station>>(
+    () => IzdenizStationConverter(),
   );
 }
 
