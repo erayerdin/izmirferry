b86408a53fad69b417397acb4c090bb2539e5141
Eray Erdin <eraygezer.94@gmail.com>
2023-05-22 15:08:30 +0300

    redesign home page

--- lib/shared/presentation/pages/home/home.page.dart
+++ lib/shared/presentation/pages/home/home.page.dart
@@ -5,8 +5,16 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import 'package:auto_route/auto_route.dart';
+import 'package:awesome_extensions/awesome_extensions.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:izmirferry/ferry/constants.dart';
+import 'package:izmirferry/ferry/logic/station/station_bloc.dart';
+import 'package:izmirferry/shared/constants.dart';
+import 'package:izmirferry/shared/presentation/pages/home/days_menu.component.dart';
+import 'package:izmirferry/shared/presentation/pages/home/stations_menu.component.dart';
 
 @RoutePage()
 class HomePage extends StatelessWidget {
@@ -14,10 +22,36 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("izmir_ferry").tr()),
-      body: Center(
-        child: const Text('hello_world').tr(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => StationBloc(scheduleRepository: GetIt.I.get())
+            ..add(
+              StationEvent.getEndStations(
+                startStation: stations.firstWhere((s) => s.id == 1),
+                day: Days.monday,
+              ),
+            ),
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(title: const Text("izmir_ferry").tr()),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const StationsMenuComponent(stations: stations),
+            BlocBuilder<StationBloc, StationState>(
+              builder: (context, state) => state.map(
+                loading: (state) => const LinearProgressIndicator()
+                    .paddingOnly(top: 16, bottom: 16),
+                loaded: (state) => StationsMenuComponent(
+                  stations: state.stations.toList(),
+                ),
+              ),
+            ),
+            const DaysMenuComponent(),
+          ],
+        ).paddingAll(16),
       ),
     );
   }
