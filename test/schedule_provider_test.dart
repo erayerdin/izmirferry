49f7c7e6611e602c7cf85ec94f443b2273d481b0
Eray Erdin <eraygezer.94@gmail.com>
2023-05-22 11:45:53 +0300

    test get valid end stations

--- test/schedule_provider_test.dart
+++ test/schedule_provider_test.dart
@@ -23,10 +23,39 @@ void main() {
       final Dio dio = GetIt.I.get();
       final dioAdapter = DioAdapter(dio: dio);
 
-      dioAdapter.onGet(
-        'https://www.izdeniz.com.tr/tr/HareketSaatleri/1/1/1',
+      dioAdapter.onGet('https://www.izdeniz.com.tr/tr/HareketSaatleri/1/1/1',
+          (server) async {
+        final file = File('test_assets/schedules_sample.html');
+        final source = await file.readAsString();
+
+        return server.reply(200, source);
+      });
+
+      dioAdapter.onPost(
+          'https://www.izdeniz.com.tr/tr/IskeleGuncelle', (server) async {});
+
+      dioAdapter.onRoute(
+        'route',
+        request: const Request(
+          method: RequestMethods.post,
+          data: {'kalkisIskele': '1', 'gunTipi': '1'},
+        ),
         (server) async {
-          final file = File('test_assets/schedules_sample.html');
+          final file = File('test_assets/endstations_valid_sample.json');
+          final source = await file.readAsString();
+
+          return server.reply(200, source);
+        },
+      );
+
+      dioAdapter.onRoute(
+        'route',
+        request: const Request(
+          method: RequestMethods.post,
+          data: {'kalkisIskele': '100', 'gunTipi': '1'},
+        ),
+        (server) async {
+          final file = File('test_assets/endstations_invalid_sample.json');
           final source = await file.readAsString();
 
           return server.reply(200, source);
@@ -36,22 +65,30 @@ void main() {
       provider = GetIt.I.get();
     });
 
-    test(
-      'get schedules',
-      () async {
-        final schedules = (await provider.getSchedules(
-          startStationId: 1,
-          endStationId: 1,
-          dayId: 1,
-        ))
-            .toList();
+    test('get schedules', () async {
+      final schedules = (await provider.getSchedules(
+        startStationId: 1,
+        endStationId: 1,
+        dayId: 1,
+      ))
+          .toList();
 
-        final firstElm = schedules.first;
-        final lastElm = schedules.last;
+      final firstElm = schedules.first;
+      final lastElm = schedules.last;
 
-        expect(firstElm, '08:25');
-        expect(lastElm, '23:10');
-      },
-    );
+      expect(firstElm, '08:25');
+      expect(lastElm, '23:10');
+    });
+
+    test('get valid end stations', () async {
+      final stations = (await provider.getEndStations(
+        startStationId: 1,
+        dayId: 1,
+      ))
+          .toList();
+
+      final String firstStationName = stations[0]['Adi'];
+      expect(firstStationName, 'Konak');
+    });
   });
 }
