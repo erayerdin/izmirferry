ffd4744ba9220a26026278ef3b3b45060f19a3da
Eray Erdin <eraygezer.94@gmail.com>
2023-05-22 12:51:50 +0300

    add StationState.loading and StationState.loaded

--- lib/ferry/logic/station/station_bloc.dart
+++ lib/ferry/logic/station/station_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:izmirferry/ferry/data/models/station/station.model.dart';
 import 'package:izmirferry/ferry/data/repositories/schedule/schedule.repository.dart';
+import 'package:izmirferry/shared/constants.dart';
 
 part 'station_bloc.freezed.dart';
 part 'station_event.dart';
@@ -10,7 +12,7 @@ class StationBloc extends Bloc<StationEvent, StationState> {
   late ScheduleRepository _scheduleRepository;
 
   StationBloc({required ScheduleRepository scheduleRepository})
-      : super(const StationState.initial()) {
+      : super(const StationState.loading()) {
     _scheduleRepository = scheduleRepository;
 
     on<StationEvent>((event, emit) {
--- lib/ferry/logic/station/station_state.dart
+++ lib/ferry/logic/station/station_state.dart
@@ -2,5 +2,8 @@ part of 'station_bloc.dart';
 
 @freezed
 class StationState with _$StationState {
-  const factory StationState.initial() = _Initial;
+  const factory StationState.loading() = _LoadingState;
+  const factory StationState.loaded({
+    required Iterable<Station> stations,
+  }) = _LoadedState;
 }
