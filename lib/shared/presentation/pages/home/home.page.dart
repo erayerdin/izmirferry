44122bafe6d29b200faf72a4a67b593748a2d1b8
Eray Erdin <eraygezer.94@gmail.com>
2023-05-22 12:56:19 +0300

    add HomePage

---
+++ lib/shared/presentation/pages/home/home.page.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 Eray Erdin
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("izmir_ferry").tr()),
+      body: Center(
+        child: const Text('hello_world').tr(),
+      ),
+    );
+  }
+}
