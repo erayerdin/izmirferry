9460cdf57d6453c1f6c5c39b09dd57cc9e822fb1
Eray Erdin <eraygezer.94@gmail.com>
2023-05-22 15:32:10 +0300

    remove ScheduleBloc

--- lib/ferry/logic/schedule/schedule_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'schedule_bloc.dart';
-
-@freezed
-class ScheduleState with _$ScheduleState {
-  const factory ScheduleState.loading() = _LoadingState;
-  const factory ScheduleState.loaded({required Iterable<String> hours}) =
-      _LoadedState;
-}
