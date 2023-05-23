1bb5fa8f515735da3884b07bcbd58aaa6c6e2632
Eray Erdin <eraygezer.94@gmail.com>
2023-05-24 00:34:38 +0300

    extract header and body

--- lib/shared/presentation/pages/home/home.page.dart
+++ lib/shared/presentation/pages/home/home.page.dart
@@ -25,17 +25,6 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final stationBloc = StationBloc(scheduleRepository: GetIt.I.get());
-    final shimmer = Shimmer.fromColors(
-      baseColor: Colors.blue[300]!,
-      highlightColor: Colors.blue[100]!,
-      child: Container(
-        height: 50,
-        decoration: BoxDecoration(
-          color: Colors.blue[200],
-          borderRadius: BorderRadius.circular(8),
-        ),
-      ).paddingAll(2),
-    );
 
     return MultiBlocProvider(
       providers: [
@@ -51,100 +40,131 @@ class HomePage extends StatelessWidget {
         ),
       ],
       child: Scaffold(
-        // appBar: AppBar(title: const Text("izmir_ferry").tr()),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            BlocBuilder<StationBloc, StationState>(
-              builder: (context, state) => AppBarComponent(
-                imagePath: state.map(
-                  loading: (state) => 'assets/locations/izmir.jpg',
-                  loaded: (state) {
-                    final StationBloc bloc = context.read();
-                    final Station endStation = bloc.currentParams['endStation'];
+            const _Header(),
+            const _Body().paddingOnly(left: 16, right: 16).expanded(),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-                    switch (endStation.name) {
-                      case 'Alsancak':
-                        return 'assets/locations/alsancak.jpg';
-                      case 'Bostanlı':
-                        return 'assets/locations/bostanli.jpg';
-                      case 'Göztepe':
-                        return 'assets/locations/goztepe.jpg';
-                      case 'Karantina':
-                        return 'assets/locations/karantina.jpg';
-                      case 'Karşıyaka':
-                        return 'assets/locations/karsiyaka.jpg';
-                      case 'Konak':
-                        return 'assets/locations/konak.jpg';
-                      case 'Pasaport':
-                        return 'assets/locations/pasaport.jpg';
-                      case 'Üçkuyular':
-                        return 'assets/locations/uckuyular.jpg';
-                      default:
-                        return 'assets/locations/izmir.jpg';
-                    }
-                  },
-                ),
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      state
-                          .map(
-                            loading: (state) => shimmer,
-                            loaded: (state) => StationsMenuComponent(
-                              stations: allStation,
-                              selectedStation:
-                                  stationBloc.currentParams['startStation'],
-                              onChanged: (station) {
-                                stationBloc.add(
-                                  StationEvent.changeStartStation(station),
-                                );
-                              },
-                            ),
-                          )
-                          .expanded(),
-                      const Icon(Icons.arrow_right_alt, color: Colors.white)
-                          .paddingOnly(left: 16, right: 16),
-                      state
-                          .map(
-                            loading: (state) => shimmer,
-                            loaded: (state) => StationsMenuComponent(
-                              stations: state.endStations.toList(),
-                              selectedStation:
-                                  stationBloc.currentParams['endStation'],
-                              onChanged: (station) {
-                                stationBloc.add(
-                                  StationEvent.changeEndStation(station),
-                                );
-                              },
-                            ),
-                          )
-                          .flexible(),
-                    ],
-                  ),
-                  state.map(
+class _Header extends StatelessWidget {
+  const _Header();
+
+  @override
+  Widget build(BuildContext context) {
+    final StationBloc stationBloc = context.read();
+    final shimmer = Shimmer.fromColors(
+      baseColor: Colors.blue[300]!,
+      highlightColor: Colors.blue[100]!,
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.blue[200],
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ).paddingAll(2),
+    );
+
+    return BlocBuilder<StationBloc, StationState>(
+      builder: (context, state) => AppBarComponent(
+        imagePath: state.map(
+          loading: (state) => 'assets/locations/izmir.jpg',
+          loaded: (state) {
+            final StationBloc bloc = context.read();
+            final Station endStation = bloc.currentParams['endStation'];
+            return _getBackgroundImage(endStation);
+          },
+        ),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              state
+                  .map(
                     loading: (state) => shimmer,
-                    loaded: (state) => DaysMenuComponent(
-                      selectedDay: stationBloc.currentParams['day'],
-                      onChanged: (day) {
-                        stationBloc.add(StationEvent.changeDay(day));
+                    loaded: (state) => StationsMenuComponent(
+                      stations: allStation,
+                      selectedStation:
+                          stationBloc.currentParams['startStation'],
+                      onChanged: (station) {
+                        stationBloc.add(
+                          StationEvent.changeStartStation(station),
+                        );
                       },
                     ),
-                  ),
-                ],
-              ),
+                  )
+                  .expanded(),
+              const Icon(Icons.arrow_right_alt, color: Colors.white)
+                  .paddingOnly(left: 16, right: 16),
+              state
+                  .map(
+                    loading: (state) => shimmer,
+                    loaded: (state) => StationsMenuComponent(
+                      stations: state.endStations.toList(),
+                      selectedStation: stationBloc.currentParams['endStation'],
+                      onChanged: (station) {
+                        stationBloc.add(
+                          StationEvent.changeEndStation(station),
+                        );
+                      },
+                    ),
+                  )
+                  .flexible(),
+            ],
+          ),
+          state.map(
+            loading: (state) => shimmer,
+            loaded: (state) => DaysMenuComponent(
+              selectedDay: stationBloc.currentParams['day'],
+              onChanged: (day) {
+                stationBloc.add(StationEvent.changeDay(day));
+              },
             ),
-            BlocBuilder<StationBloc, StationState>(
-              builder: (context, state) => state.map(
-                loading: (state) =>
-                    const CircularProgressIndicator().toCenter(),
-                loaded: (state) => SchedulesListComponent(
-                  schedules: state.schedules.toList(),
-                ),
-              ),
-            ).paddingOnly(left: 16, right: 16).expanded(),
-          ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _getBackgroundImage(Station station) {
+    switch (station.name) {
+      case 'Alsancak':
+        return 'assets/locations/alsancak.jpg';
+      case 'Bostanlı':
+        return 'assets/locations/bostanli.jpg';
+      case 'Göztepe':
+        return 'assets/locations/goztepe.jpg';
+      case 'Karantina':
+        return 'assets/locations/karantina.jpg';
+      case 'Karşıyaka':
+        return 'assets/locations/karsiyaka.jpg';
+      case 'Konak':
+        return 'assets/locations/konak.jpg';
+      case 'Pasaport':
+        return 'assets/locations/pasaport.jpg';
+      case 'Üçkuyular':
+        return 'assets/locations/uckuyular.jpg';
+      default:
+        return 'assets/locations/izmir.jpg';
+    }
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<StationBloc, StationState>(
+      builder: (context, state) => state.map(
+        loading: (state) => const CircularProgressIndicator().toCenter(),
+        loaded: (state) => SchedulesListComponent(
+          schedules: state.schedules.toList(),
         ),
       ),
     );
