caf97a74c7aa914eda65bce3022c6512c344751e
Eray Erdin (&mut self) <eraygezer.94@gmail.com>
2023-05-25 00:04:21 +0300

    Refactor Background Asset Path Code (#6)
    * add Station.backgroundAssetPath getter
    
    * refactor references
    
    * update changelog
    
    * fix file name extension to jpg

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog],
 and this project adheres to [Semantic Versioning].
 
+## [Unreleased]
+
+### For Users
+
+### For Developers
+
+#### Changed
+
+ - Add `Station.backgroundAssetPath` and refactor references
+
 ## [0.2.0] - 24.05.2023
 
 ### Added
--- lib/ferry/data/models/station/station.model.dart
+++ lib/ferry/data/models/station/station.model.dart
@@ -15,4 +15,26 @@ class Station with _$Station {
     required int id,
     required String name,
   }) = _Station;
+
+  String? get backgroundAssetPath {
+    switch (id) {
+      case 1:
+        return 'assets/locations/konak.jpg';
+      case 2:
+        return 'assets/locations/karsiyaka.jpg';
+      case 3:
+        return 'assets/locations/bostanli.jpg';
+      case 4:
+        return 'assets/locations/pasaport.jpg';
+      case 5:
+        return 'assets/locations/alsancak.jpg';
+      case 6:
+        return 'assets/locations/goztepe.jpg';
+      case 7:
+        return 'assets/locations/uckuyular.jpg';
+      case 1019:
+        return 'assets/locations/karantina.jpg';
+    }
+    return null;
+  }
 }
