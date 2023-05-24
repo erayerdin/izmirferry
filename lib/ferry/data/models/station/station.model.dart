95c44a9275f2b5010764af3fb81d3ea2c03b1ec0
Eray Erdin <eraygezer.94@gmail.com>
2023-05-24 22:50:27 +0300

    add Station.backgroundAssetPath getter

--- lib/ferry/data/models/station/station.model.dart
+++ lib/ferry/data/models/station/station.model.dart
@@ -15,4 +15,26 @@ class Station with _$Station {
     required int id,
     required String name,
   }) = _Station;
+
+  String? get backgroundAssetPath {
+    switch (id) {
+      case 1:
+        return 'assets/locations/konak.png';
+      case 2:
+        return 'assets/locations/karsiyaka.png';
+      case 3:
+        return 'assets/locations/bostanli.png';
+      case 4:
+        return 'assets/locations/pasaport.png';
+      case 5:
+        return 'assets/locations/alsancak.png';
+      case 6:
+        return 'assets/locations/goztepe.png';
+      case 7:
+        return 'assets/locations/uckuyular.png';
+      case 1019:
+        return 'assets/locations/karantina.png';
+    }
+    return null;
+  }
 }
