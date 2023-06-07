ad4b14b36d65accbe12bf21e0d99c6f6b02110b6
Eray Erdin <eraygezer.94@gmail.com>
2023-06-07 18:53:39 +0300

    redesign SchedulesListComponent

--- lib/shared/presentation/pages/home/schedules_list.component.dart
+++ lib/shared/presentation/pages/home/schedules_list.component.dart
@@ -8,17 +8,24 @@ import 'package:awesome_extensions/awesome_extensions.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:izmirferry/ferry/data/models/station/station.model.dart';
 import 'package:izmirferry/ferry/logic/station/station_bloc.dart';
 import 'package:izmirferry/shared/constants.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class SchedulesListComponent extends StatelessWidget {
+  final Station startStation;
+  final Station endStation;
+  final Day day;
   final List<String> schedules;
   final String? nextSchedule;
   final _itemScrollController = ItemScrollController();
 
   SchedulesListComponent({
     Key? key,
+    required this.startStation,
+    required this.endStation,
+    required this.day,
     required this.schedules,
     this.nextSchedule,
   }) : super(key: key);
@@ -39,19 +46,25 @@ class SchedulesListComponent extends StatelessWidget {
       }
     });
 
-    return ScrollablePositionedList.separated(
-      itemScrollController: _itemScrollController,
-      itemBuilder: (context, index) {
-        final schedule = schedules[index];
-        return Text(
-          schedule,
-          style: schedule == nextSchedule
-              ? context.textTheme.displayLarge?.copyWith(color: Colors.blue)
-              : context.textTheme.displayLarge,
-        ).toCenter();
-      },
-      separatorBuilder: (context, index) => 8.heightBox,
-      itemCount: schedules.length,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        ScrollablePositionedList.separated(
+          itemScrollController: _itemScrollController,
+          itemBuilder: (context, index) {
+            final schedule = schedules[index];
+            return Text(
+              schedule,
+              style: schedule == nextSchedule
+                  ? context.textTheme.displayLarge?.copyWith(color: Colors.blue)
+                  : context.textTheme.displayLarge,
+            ).toCenter();
+          },
+          separatorBuilder: (context, index) => 8.heightBox,
+          itemCount: schedules.length,
+          padding: const EdgeInsets.all(16),
+        ).expanded(),
+      ],
     );
   }
 }
