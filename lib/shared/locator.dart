18fdb9fb11a24f4010f25bc938927632622085e5
Eray Erdin <eraygezer.94@gmail.com>
2023-06-09 16:20:42 +0300

    configure cache on locator

--- lib/shared/locator.dart
+++ lib/shared/locator.dart
@@ -6,8 +6,10 @@
 
 import 'dart:convert';
 
+import 'package:awesome_extensions/awesome_extensions.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:flutter_loggy_dio/flutter_loggy_dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:izmirferry/ferry/data/converters/station/station.converter.dart';
@@ -16,34 +18,48 @@ import 'package:izmirferry/ferry/data/providers/schedule/schedule.provider.dart'
 import 'package:izmirferry/ferry/data/providers/station/station.provider.dart';
 import 'package:izmirferry/ferry/data/repositories/schedule/schedule.repository.dart';
 import 'package:izmirferry/ferry/data/repositories/station/station.repository.dart';
+import 'package:path_provider/path_provider.dart';
 
 void initLocator() {
   //-----//
   // Dio //
   //-----//
-  GetIt.I.registerLazySingleton<DioCacheInterceptor>(
-    () => DioCacheInterceptor(
-      options: CacheOptions(
-        store: MemCacheStore(),
-      ),
-    ),
+  GetIt.I.registerLazySingletonAsync<DioCacheInterceptor>(
+    () async {
+      final appSupportDir = await getApplicationSupportDirectory();
+      return DioCacheInterceptor(
+        options: CacheOptions(
+          store: HiveCacheStore(
+            appSupportDir.path,
+            hiveBoxName: 'izdeniz_client_cache',
+          ),
+          allowPostMethod: true,
+          maxStale: 6.hours,
+        ),
+      );
+    },
   );
 
-  GetIt.I.registerLazySingleton<Dio>(
-    () => Dio()
+  GetIt.I.registerLazySingletonAsync<Dio>(
+    () async => Dio()
       ..interceptors.addAll(
-        [GetIt.I.get<DioCacheInterceptor>(), LoggyDioInterceptor()],
+        [await GetIt.I.getAsync<DioCacheInterceptor>(), LoggyDioInterceptor()],
       ),
+    instanceName: 'izdeniz_client',
   );
 
   //-----------//
   // Providers //
   //-----------//
-  GetIt.I.registerLazySingleton<ScheduleProvider>(
-    () => IzdenizScheduleProvider(client: GetIt.I.get()),
+  GetIt.I.registerLazySingletonAsync<ScheduleProvider>(
+    () async => IzdenizScheduleProvider(
+      client: await GetIt.I.getAsync(instanceName: 'izdeniz_client'),
+    ),
   );
-  GetIt.I.registerLazySingleton<StationProvider>(
-    () => IzdenizStationProvider(client: GetIt.I.get()),
+  GetIt.I.registerLazySingletonAsync<StationProvider>(
+    () async => IzdenizStationProvider(
+      client: await GetIt.I.getAsync(instanceName: 'izdeniz_client'),
+    ),
   );
 
   //------------//
@@ -56,12 +72,13 @@ void initLocator() {
   //--------------//
   // Repositories //
   //--------------//
-  GetIt.I.registerLazySingleton<ScheduleRepository>(
-    () => IzdenizScheduleRepository(scheduleProvider: GetIt.I.get()),
+  GetIt.I.registerLazySingletonAsync<ScheduleRepository>(
+    () async =>
+        IzdenizScheduleRepository(scheduleProvider: await GetIt.I.getAsync()),
   );
-  GetIt.I.registerLazySingleton<StationRepository>(
-    () => IzdenizStationRepository(
-      stationProvider: GetIt.I.get(),
+  GetIt.I.registerLazySingletonAsync<StationRepository>(
+    () async => IzdenizStationRepository(
+      stationProvider: await GetIt.I.getAsync(),
       rawToStationConverter: GetIt.I.get(),
     ),
   );
