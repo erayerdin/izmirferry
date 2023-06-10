f78daf4437f10b5ced963fea22a652e6fea87f64
Eray Erdin <eraygezer.94@gmail.com>
2023-06-10 06:26:35 +0300

    change favorites to positional arg

--- lib/favorite/logic/favorite/favorite_state.dart
+++ lib/favorite/logic/favorite/favorite_state.dart
@@ -3,7 +3,7 @@ part of 'favorite_bloc.dart';
 @freezed
 class FavoriteState with _$FavoriteState {
   const factory FavoriteState.loading() = _LoadingState;
-  const factory FavoriteState.listed({required Iterable<Favorite> favorites}) =
+  const factory FavoriteState.listed(Iterable<Favorite> favorites) =
       _ListedState;
   const factory FavoriteState.added() = _AddedState;
   const factory FavoriteState.deleted() = _DeletedState;
