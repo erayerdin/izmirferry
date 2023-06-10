109ef2d1fea1f595fad2403e6ca5a88a7ca649a0
Eray Erdin <eraygezer.94@gmail.com>
2023-06-10 05:45:14 +0300

    add FavoriteProvider to service locator

--- lib/shared/locator.dart
+++ lib/shared/locator.dart
@@ -12,6 +12,7 @@ import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:flutter_loggy_dio/flutter_loggy_dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:izmirferry/favorite/data/providers/favorite/favorite.provider.dart';
 import 'package:izmirferry/ferry/data/converters/station/station.converter.dart';
 import 'package:izmirferry/ferry/data/models/station/station.model.dart';
 import 'package:izmirferry/ferry/data/providers/schedule/schedule.provider.dart';
@@ -70,6 +71,9 @@ void initLocator() {
     () async => IzdenizStationProvider(
       client: await GetIt.I.getAsync(instanceName: 'izdeniz_client'),
     ),
+  );
+  GetIt.I.registerLazySingletonAsync<FavoriteProvider>(
+    () async => SqliteFavoriteProvider(database: await GetIt.I.getAsync()),
   );
 
   //------------//
