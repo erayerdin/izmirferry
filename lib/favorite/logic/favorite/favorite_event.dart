226870bd83797305b23bf105cc792262998cb5e2
Eray Erdin <eraygezer.94@gmail.com>
2023-06-10 06:27:00 +0300

    add args to events

--- lib/favorite/logic/favorite/favorite_event.dart
+++ lib/favorite/logic/favorite/favorite_event.dart
@@ -2,7 +2,11 @@ part of 'favorite_bloc.dart';
 
 @freezed
 class FavoriteEvent with _$FavoriteEvent {
-  const factory FavoriteEvent.add() = _AddEvent;
+  const factory FavoriteEvent.add({
+    required Station startStation,
+    required Station endStation,
+    required Day? day,
+  }) = _AddEvent;
   const factory FavoriteEvent.list() = _ListEvent;
-  const factory FavoriteEvent.delete() = _DeleteEvent;
+  const factory FavoriteEvent.delete(Station station) = _DeleteEvent;
 }
