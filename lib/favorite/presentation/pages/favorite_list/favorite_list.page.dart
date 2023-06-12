0aa105ba11987ffdd6b3ed90e587fcd557443e68
Eray Erdin <eraygezer.94@gmail.com>
2023-06-13 00:48:08 +0300

    wip: implement basic state on FavoriteListPage

--- lib/favorite/presentation/pages/favorite_list/favorite_list.page.dart
+++ lib/favorite/presentation/pages/favorite_list/favorite_list.page.dart
@@ -5,8 +5,13 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import 'package:auto_route/auto_route.dart';
+import 'package:awesome_extensions/awesome_extensions.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it_future_builder/get_it_future_builder.dart';
+import 'package:izmirferry/favorite/data/repository/favorite/favorite.repository.dart';
+import 'package:izmirferry/favorite/logic/favorite/favorite_bloc.dart';
 
 @RoutePage()
 class FavoriteListPage extends StatelessWidget {
@@ -14,10 +19,46 @@ class FavoriteListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('favorites').tr(),
-      ),
-    );
+    return GetItFutureBuilder<FavoriteRepository>(
+        loading: (context) => Scaffold(
+              body: const CircularProgressIndicator().toCenter(),
+            ),
+        ready: (context, favoriteRepo) {
+          return MultiBlocProvider(
+            providers: [
+              BlocProvider(
+                create: (context) =>
+                    FavoriteBloc(favoriteRepository: favoriteRepo)
+                      ..add(const FavoriteEvent.list()),
+              )
+            ],
+            child: Scaffold(
+              appBar: AppBar(
+                title: const Text('favorites').tr(),
+              ),
+              body: BlocBuilder<FavoriteBloc, FavoriteState>(
+                builder: (context, state) {
+                  return state.map(
+                    loading: (state) =>
+                        const CircularProgressIndicator().toCenter(),
+                    listed: (state) {
+                      final favorites = state.favorites.toList(growable: false);
+
+                      if (favorites.isEmpty) {
+                        return const Text('no_favorites_found').tr().toCenter();
+                      }
+
+                      return const Text('tbi'); // TODO tbi
+                    },
+                    added: (state) =>
+                        const CircularProgressIndicator().toCenter(),
+                    deleted: (state) =>
+                        const CircularProgressIndicator().toCenter(),
+                  );
+                },
+              ),
+            ),
+          );
+        });
   }
 }
