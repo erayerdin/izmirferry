d9441ce4b6e479a8357ba081e5129817f51f8073
Eray Erdin <eraygezer.94@gmail.com>
2023-06-14 11:21:03 +0300

    remove FavoriteRepository.check

--- lib/favorite/data/repository/favorite/favorite.repository.dart
+++ lib/favorite/data/repository/favorite/favorite.repository.dart
@@ -8,19 +8,13 @@ import 'dart:convert';
 
 import 'package:izmirferry/favorite/data/models/favorite/favorite.model.dart';
 import 'package:izmirferry/favorite/data/providers/favorite/favorite.provider.dart';
-import 'package:izmirferry/ferry/data/models/station/station.model.dart';
 import 'package:izmirferry/shared/constants.dart';
 import 'package:izmirferry/shared/logger.dart';
 
 abstract class FavoriteRepository with DataLoggy {
   Future<int> add(Favorite favorite);
 
-  /// returns id if present
-  Future<int?> check({
-    required Station startStation,
-    required Station endStation,
-    Day? day,
-  });
+  // TODO add check method to check if a favorite entry exists
 
   Future<Iterable<Favorite>> list();
 
@@ -48,24 +42,6 @@ class SqliteFavoriteRepository extends FavoriteRepository {
       startStationId: favorite.startStation.id,
       endStationId: favorite.endStation.id,
       dayId: favorite.day?.id,
-    );
-  }
-
-  @override
-  Future<int?> check({
-    required Station startStation,
-    required Station endStation,
-    Day? day,
-  }) async {
-    loggy.debug('Checking favorite...');
-    loggy.trace('start station: $startStation');
-    loggy.trace('end station: $endStation');
-    loggy.trace('day: $day');
-
-    return await _favoriteProvider.check(
-      startStationId: startStation.id,
-      endStationId: endStation.id,
-      dayId: day?.id,
     );
   }
 
