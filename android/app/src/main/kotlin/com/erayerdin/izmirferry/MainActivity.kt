ef1de180eb71d55bfa1a7a9ccffd0f75de24a821
Eray Erdin <eraygezer.94@gmail.com>
2024-02-28 01:19:23 +0300

    reset project

--- android/app/src/main/kotlin/com/erayerdin/izmirferry/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.erayerdin.izmirferry
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/favorite/logic/add_favorite_form/add_favorite_form_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'add_favorite_form_bloc.dart';
-
-@freezed
-class AddFavoriteFormState with _$AddFavoriteFormState {
-  const factory AddFavoriteFormState.processing() = _ProcessingState;
-  const factory AddFavoriteFormState.valid({required Favorite favorite}) =
-      _ValidState;
-}
